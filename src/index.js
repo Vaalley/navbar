1b721ad1b84bc19ff590e1da2f30bc1d508b494a
Valley <vaaleyou@gmail.com>
2023-01-28 22:58:57 +0100

    navbar almost done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 let menuButton = document.getElementsByClassName('menu')[0];
+navShortcutsMobile = document.getElementsByClassName('nav-shortcuts-mobile')[0];
 
+// when the menu button is clicked, toggle display the nav-shortcuts-mobile
+menuButton.addEventListener('click', function () {
+    navShortcutsMobile.classList.toggle('show');
+}
+);
